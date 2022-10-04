9823208a02355d39980cdc6eb571d7869a589083
Ilya Tregubov <ilya@moodle.com>
2022-10-04 16:12:57 +0400

    MDL-75455 gradereport_singleview: Add Bulk actions menu.

--- grade/report/singleview/lib.php
+++ grade/report/singleview/lib.php
@@ -175,5 +175,34 @@ class gradereport_singleview extends grade_report {
 
         return $output;
     }
+
+    /**
+     * Adds bulk actions menu.
+     *
+     * @param renderer_base $output
+     * @return string HTML to display
+     */
+    public function bulk_actions_menu(renderer_base $output) : string {
+        $options = [
+            'overrideallgrades' => get_string('overrideallgrades', 'gradereport_singleview'),
+            'overridenonegrades' => get_string('overridenonegrades', 'gradereport_singleview'),
+            'excludeallgrades' => get_string('excludeallgrades', 'gradereport_singleview'),
+            'excludenonegrades' => get_string('excludenonegrades', 'gradereport_singleview'),
+            'bulklegend' => get_string('bulklegend', 'gradereport_singleview')
+        ];
+
+        $menu = new \action_menu();
+        $menu->set_menu_trigger(get_string('actions'));
+
+        foreach ($options as $type => $option) {
+            $action = new \action_menu_link_secondary(new \moodle_url('#'), null, $option,
+                ['data-role' => $type]);
+            $menu->add($action);
+        }
+        $menu->attributes['class'] .= ' float-left mr-1';
+
+        return $output->render($menu);
+    }
+
 }
 
