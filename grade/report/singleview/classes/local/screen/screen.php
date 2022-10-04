047e9d00a206be117adf9e46a2c2c26ea82d1979
Ilya Tregubov <ilya@moodle.com>
2022-10-04 16:22:28 +0400

    MDL-75455 gradereport_singleview: Fix edit mode.
    Switching range column with Grade column had side effect.
    Essential columns are grade, feedback, override, exclude.
    They come as pack and you can't just insert columns between those 4.

--- grade/report/singleview/classes/local/screen/screen.php
+++ grade/report/singleview/classes/local/screen/screen.php
@@ -285,6 +285,7 @@ abstract class screen {
         $warnings = array();
 
         $fields = $this->definition();
+        $fields[] = 'finalgrade';
 
         // Avoiding execution timeouts when updating
         // a large amount of grades.
