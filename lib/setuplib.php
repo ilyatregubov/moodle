ecfd4099209bc58f3e245c6912f03d279b9a8bcf
Ilya Tregubov <ilya@moodle.com>
2022-10-19 12:56:02 +0300

    MDL-76033 core: bump version in is_major_upgrade_required
    MDL-58266 changes db structure. So upgrade must happen before
    accessing any page.

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1411,7 +1411,7 @@ function disable_output_buffering() {
  */
 function is_major_upgrade_required() {
     global $CFG;
-    $lastmajordbchanges = 2022022200.00;
+    $lastmajordbchanges = 2022101400.01;
 
     $required = empty($CFG->version);
     $required = $required || (float)$CFG->version < $lastmajordbchanges;
