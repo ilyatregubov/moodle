8ec65fbc62da9b53a41a8b04a1bc61ba637cec6f
Ilya Tregubov <ilya@moodle.com>
2023-01-17 09:59:56 +0800

    MDL-75719 completion: Fix viewed criteria.
    When we unlock completion criteria and save changes 'viewed' criteria
    still should be completed if user has viewed activity before. Seems
    that that was broken by recent changes. Fixing here.

--- lib/completionlib.php
+++ lib/completionlib.php
@@ -1110,6 +1110,9 @@ class completion_info {
 
                 if (empty($data->coursemoduleid)) {
                     $cacheddata[$data->cmid] = $defaultdata;
+                    if ($data->viewed) {
+                        $cacheddata[$data->cmid]['viewed'] = $data->viewed;
+                    }
                     $cacheddata[$data->cmid]['coursemoduleid'] = $data->cmid;
                 } else {
                     unset($data->cmid);
