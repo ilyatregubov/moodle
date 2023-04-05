b11a4658ec2b659e2f759b67438a8ce5a54d3755
Ilya Tregubov <ilya@moodle.com>
2023-04-05 11:23:32 +0800

    fix

--- grade/lib.php
+++ grade/lib.php
@@ -2548,7 +2548,7 @@ class grade_structure {
 
                 // Sorting item.
                 if ($baseurl) {
-                    $sortlink = $baseurl;
+                    $sortlink = clone($baseurl);
                     if (isset($element['object']->id)) {
                         $sortlink->param('sortitemid', $element['object']->id);
                     } else if ($element['type'] == 'userfield') {
