07288a6c7473068b321444ef314c43a632e30bf6
Ilya Tregubov <ilya@moodle.com>
2023-08-01 11:38:00 +0800

    MDL-43820 enrol_guest: Override find_instance
    For guest enrol method there can be just one instance.
    So we can just match by type

--- enrol/guest/lib.php
+++ enrol/guest/lib.php
@@ -489,6 +489,26 @@ class enrol_guest_plugin extends enrol_plugin {
         return $errors;
     }
 
+    /**
+     * Finds matching instances for a given course.
+     *
+     * @param array $enrolmentdata enrolment data.
+     * @param int|null $courseid Course ID.
+     * @return stdClass|null Matching instance
+     */
+    public function find_instance(array $enrolmentdata, ?int $courseid = null) : ?stdClass {
+
+        $instances = enrol_get_instances($courseid, false);
+        $instance = null;
+        foreach ($instances as $i) {
+            if ($i->enrol == 'guest') {
+                // There can be only one guest enrol instance so find first available.
+                $instance = $i;
+                break;
+            }
+        }
+        return $instance;
+    }
 
 }
 
