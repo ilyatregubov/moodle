d8a20ffb66e629d61787f15ec3dba854f0c0dab0
Ilya Tregubov <ilya@moodle.com>
2023-08-01 11:35:45 +0800

    MDL-43820 enrol: Add method to find plugin enrol instance
    When upload courses we need to match instance with provided data.

--- lib/enrollib.php
+++ lib/enrollib.php
@@ -3347,4 +3347,18 @@ abstract class enrol_plugin {
         }
         return $errors;
     }
+
+    /**
+     * Finds matching instances for a given course.
+     *
+     * @param array $enrolmentdata enrolment data.
+     * @param int $courseid Course ID.
+     * @return stdClass|null Matching instance
+     */
+    public function find_instance(array $enrolmentdata, int $courseid) : ?stdClass {
+
+        // By default, we assume we can't uniquely identify an instance so better not update any.
+        // Plugins can override this if they can uniquely identify an instance.
+        return null;
+    }
 }
