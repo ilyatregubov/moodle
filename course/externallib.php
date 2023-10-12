551785ffbcce2a9f98f8306f6ca0485f8b1e2f08
Ilya Tregubov <ilya@moodle.com>
2023-10-12 16:02:02 +0800

    MDL-68652 course: Modify webservice to show only gradable users
    core_course_get_enrolled_users_by_cmid now takes extra parameter
    if we want to get all users or only gradable users.

--- course/externallib.php
+++ course/externallib.php
@@ -4233,6 +4233,8 @@ class core_course_external extends external_api {
             'groupid' => new external_value(PARAM_INT, 'id of the group', VALUE_DEFAULT, 0),
             'onlyactive' => new external_value(PARAM_BOOL, 'whether to return only active users or all.',
                 VALUE_DEFAULT, false),
+            'onlygradable' => new external_value(PARAM_BOOL, 'whether to return only gradable users or all.',
+                VALUE_DEFAULT, false),
         ]);
     }
 
@@ -4242,32 +4244,40 @@ class core_course_external extends external_api {
      * @param int $cmid Course Module id from which the users will be obtained
      * @param int $groupid Group id from which the users will be obtained
      * @param bool $onlyactive Whether to return only the active enrolled users or all enrolled users in the course.
+     * @param bool $onlygradable Whether to return only the gradable users in the course.
      * @return array List of users
      * @throws invalid_parameter_exception
      */
-    public static function get_enrolled_users_by_cmid(int $cmid, int $groupid = 0, bool $onlyactive = false) {
-    global $PAGE;
+    public static function get_enrolled_users_by_cmid(int $cmid, int $groupid = 0,
+        bool $onlyactive = false, bool $onlygradable = false) {
+        global $PAGE, $CFG;
+        require_once($CFG->dirroot . '/grade/lib.php');
+
         $warnings = [];
 
         self::validate_parameters(self::get_enrolled_users_by_cmid_parameters(), [
-                'cmid' => $cmid,
-                'groupid' => $groupid,
-                'onlyactive' => $onlyactive,
+            'cmid' => $cmid,
+            'groupid' => $groupid,
+            'onlyactive' => $onlyactive,
         ]);
 
         list($course, $cm) = get_course_and_cm_from_cmid($cmid);
         $coursecontext = context_course::instance($course->id);
         self::validate_context($coursecontext);
 
-        $enrolledusers = get_enrolled_users($coursecontext, '', $groupid, 'u.*', null, 0, 0, $onlyactive);
-
+        if (!$onlygradable) {
+            $userstmp = get_enrolled_users($coursecontext, '', $groupid, 'u.*', null, 0, 0, $onlyactive);
+        } else {
+            $userstmp = get_gradable_users($course->id, $groupid, $onlyactive);
+        }
         $users = array_map(function ($user) use ($PAGE) {
             $user->fullname = fullname($user);
             $userpicture = new user_picture($user);
             $userpicture->size = 1;
             $user->profileimage = $userpicture->get_url($PAGE)->out(false);
             return $user;
-        }, $enrolledusers);
+        }, $userstmp);
+
         sort($users);
 
         return [
