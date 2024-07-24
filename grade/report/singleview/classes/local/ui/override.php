89d4818bb96d4a0398274b7b513f78c13a13bc9b
Ilya Tregubov <ilya.a.tregubov@gmail.com>
2024-07-24 11:49:27 +0800

    MDL-81945 gradereport_singleview: Fix hidden grades handling.
    If a user is lacking moodle/grade:viewhidden he should not be able
    to view or modify grades.

--- grade/report/singleview/classes/local/ui/override.php
+++ grade/report/singleview/classes/local/ui/override.php
@@ -24,6 +24,8 @@
 
 namespace gradereport_singleview\local\ui;
 
+use context_course;
+
 defined('MOODLE_INTERNAL') || die;
 
 /**
@@ -94,7 +96,12 @@ class override extends grade_attribute_format implements be_checked, be_disabled
      * @return element
      */
     public function determine_format(): element {
-        if (!$this->grade->grade_item->is_overridable_item()) {
+        $canviewhidden = true;
+        if (($this->grade->is_hidden() || $this->grade->grade_item->is_hidden()) &&
+            !has_capability('moodle/grade:viewhidden', context_course::instance($this->grade->grade_item->courseid))) {
+            $canviewhidden = false;
+        }
+        if (!$this->grade->grade_item->is_overridable_item() || !$canviewhidden) {
             return new empty_element();
         }
         return new checkbox_attribute(
